a66c675ecf6ea99d60a4904750cacada9a225375
gevann <gevann.mullins@gmail.com>
2020-08-30 14:57:48 +0200

    Testing the dbConnectQuery class

--- scripts/submit_exchange_rate.php
+++ scripts/submit_exchange_rate.php
@@ -2,6 +2,12 @@
 
 // add the currency exchange api class to the file
 include_once "../classess/CurrencyExchangeAPI.php";
+include_once "../classess/DbConnectQuery.php";
+
+// DbConnectQuery
+$dcq = new DbConnectQuery("localhost", "admin", "admin", "exchange_rate_api", "3306");
+
+// currencyExchangeAPI
 $cea = new CurrencyExchangeAPI();
 
 // get the form values
@@ -11,7 +17,9 @@ $baseCurrency = $_POST["baseCurrency"];
 
 $result = $cea->getDateRangeCurrencyExchange($startDate, $endDate, $baseCurrency);
 
-var_dump($result);
+//var_dump($result);
 
+//echo json_decode($result)[0];
+echo $result;
 
 
